f59aa18ef4baf3ee290046e91b9f8f25c7ea5546
QiJi- <phamphat2262@gmail.com>
2025-05-06 19:51:08 +0700

    observer

---
+++ Assets/01 Scripts/Publisher.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Publisher : MonoBehaviour
+{
+    static Dictionary<string, List<Action<object[]>>> Listeners
+         = new Dictionary<string, List<Action<object[]>>>();
+
+    public static void AddListeners(string name, Action<object[]> callback)
+    {
+        if (!Listeners.ContainsKey(name))
+        {
+            Listeners.Add(name, new List<Action<object[]>>());
+        }
+
+        Listeners[name].Add(callback);
+    }
+
+    public static void RemoveListeners(string name, Action<object[]> callback)
+    {
+        if (!Listeners.ContainsKey(name))
+        {
+            return;
+        }
+
+        Listeners[name].Remove(callback);
+    }
+
+    public static void Notify(string name, params object[] datas)
+    {
+        if (!Listeners.ContainsKey(name))
+        {
+            return;
+        }
+
+        foreach (var action in Listeners[name])
+        {
+            try
+            {
+                action?.Invoke(datas);
+            }
+            catch (Exception e)
+            {
+                Debug.LogError("Error on invoke:" + e);
+            }
+        }
+    }
+}
